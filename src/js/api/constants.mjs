638df6f4dc347acc6c53e11afa4b02a4fdda6681
VaNomad <sjur.hassel@gmail.com>
2023-02-24 11:47:29 +0000

    Adds correct host url

--- src/js/api/constants.mjs
+++ src/js/api/constants.mjs
@@ -1,3 +1,3 @@
-export const API_HOST_URL = 'https://api.noroff.dev';
+export const API_HOST_URL = 'https://nf-api.onrender.com';
 export const API_BASE_URL = '/api/v1/auction';
 export const API_AUCTION_URL = `${API_HOST_URL}${API_BASE_URL}`;
