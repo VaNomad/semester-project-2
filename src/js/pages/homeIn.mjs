3a3ed2676c4e972684bb20f3d64d450bb33d51a6
VaNomad <sjur.hassel@gmail.com>
2023-03-01 14:50:19 +0000

    adds listing style updates

--- src/js/pages/homeIn.mjs
+++ src/js/pages/homeIn.mjs
@@ -1,5 +1,9 @@
-import { listings_URL } from "../api/constants.mjs";
-import { displayLoginSuccess } from "../ui/displayError.mjs";
+import {
+  listings_URL
+} from "../api/constants.mjs";
+import {
+  displayLoginSuccess
+} from "../ui/displayError.mjs";
 
 const listingItems = document.querySelector("#listingItemsIn");
 const loginMsg = document.querySelector("#loginMsg");
@@ -39,14 +43,18 @@ export async function listingCardsIn() {
             
       <div class="col-lg-4 col-md-6 col-xs-12 text-white-50 p-0">
         <div class="card border-0 bg-secondary bg-opacity-75 m-3 p-3">
-          <img src="${
-            listing.media[0]
-          }" class="card-image" onerror="if (this.src != '/assets/vectors/heartLogo_purple.png') this.src = '/assets/vectors/heartLogo_purple.png';" alt="Listing item image">
-          <div class="card-img-overlay d-flex m-3">
-            <div class="heart">
-              <img src="/assets/vectors/heartLogo_green.png" width="30" alt="" />
+            <div class="">
+              <a href="/specific.html">
+                <img src="${listing.media[0]}" class="card-image" onerror="if (this.src != '/assets/vectors/heartLogo_purple.png') this.src = '/assets/vectors/heartLogo_purple.png';" alt="Listing item image">
+                <div class="card-img-overlay d-flex m-3"></div>
+              </a>
             </div>
-            <p class="text-dark ms-1 fs-5">${listing._count.bids}</p>
+            <div class="heart d-flex">
+              <div class="">
+              <img src="/assets/vectors/heartLogo_green.png" width="30" alt="" />
+              </div>
+              <p class="text-dark ms-1 fs-5">${listing._count.bids}</p>
+            </div>
           </div>
           <div class="card-body bg-primary m-3">
             <h5 class="card-title text-white">${listing.title}</h5>
@@ -58,21 +66,7 @@ export async function listingCardsIn() {
             <div><small class="text-danger"> AUCTION ENDS: ${new Date(listing.endsAt).toLocaleDateString()} @ ${new Date(listing.endsAt).toLocaleTimeString()}</small></div>
             <div><small class="text-muted"> LAST UPDATED: ${new Date(listing.updated).toLocaleDateString()}</small></div>
           </div>
-          <form class="d-flex justify-content-evenly bg-primary rounded align-content-end my-2 mx-3">
-            <div class="mb-3 w-25 align-bottom">
-              <label for="userBid" class="form-label">BID</label>
-              <input type="number" class="form-control" id="user-bid" aria-describedby="userBid" />
-              <div id="biddingHelp" class="form-text">Add amount in CYBID credits</div>
-            </div>
-            <div class="mb-3 w-25 align-bottom">
-              <label for="availableCredit" class="form-label"><img class="w-25"
-                  src="/assets/vectors/heartLogo_green.png" alt="Cybid credit icon" />
-                CREDITS</label>
-              <input type="text" class="form-control" id="cybidCredits" />
-            </div>
-              <button type="submit" class="btn btn-primary">Submit</button>
-              <button class="btn btn-outline-success m-3" type="submit">BID</button>
-          </form>
+          
         </div>
       </div>
           
@@ -94,3 +88,20 @@ export function loginSuccess() {
     }, 3000)
   }
 }
+
+//Rest of form
+/* <form class="d-flex justify-content-evenly bg-primary rounded align-content-end my-2 mx-3">
+            <div class="mb-3 w-25 align-bottom">
+              <label for="userBid" class="form-label">BID</label>
+              <input type="number" class="form-control" id="user-bid" aria-describedby="userBid" />
+              <div id="biddingHelp" class="form-text">Add amount in CYBID credits</div>
+            </div>
+            <div class="mb-3 w-25 align-bottom">
+              <label for="availableCredit" class="form-label"><img class="w-25"
+                  src="/assets/vectors/heartLogo_green.png" alt="Cybid credit icon" />
+                CREDITS</label>
+              <input type="text" class="form-control" id="cybidCredits" />
+            </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+              <button class="btn btn-outline-success m-3" type="submit">BID</button>
+          </form> */
