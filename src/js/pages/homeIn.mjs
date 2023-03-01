2f7038d87c9c4417f567cb78ffd7f61d2a0d501c
VaNomad <sjur.hassel@gmail.com>
2023-03-01 16:27:31 +0000

    Adds link tag to cards

--- src/js/pages/homeIn.mjs
+++ src/js/pages/homeIn.mjs
@@ -43,30 +43,30 @@ export async function listingCardsIn() {
             
       <div class="col-lg-4 col-md-6 col-xs-12 text-white-50 p-0">
         <div class="card border-0 bg-secondary bg-opacity-75 m-3 p-3">
+          <a href="/indexIn.html?_id">
             <div class="">
-              <a href="/specific.html">
+              <div>
                 <img src="${listing.media[0]}" class="card-image" onerror="if (this.src != '/assets/vectors/heartLogo_purple.png') this.src = '/assets/vectors/heartLogo_purple.png';" alt="Listing item image">
                 <div class="card-img-overlay d-flex m-3"></div>
-              </a>
-            </div>
-            <div class="heart d-flex">
-              <div class="">
-              <img src="/assets/vectors/heartLogo_green.png" width="30" alt="" />
               </div>
-              <p class="text-dark ms-1 fs-5">${listing._count.bids}</p>
+              <div class="heart d-flex">
+                <div class="">
+                  <img src="/assets/vectors/heartLogo_green.png" width="30" alt="" />
+                </div>
+                <p class="text-dark ms-1 fs-5">${listing._count.bids}</p>
+              </div>
             </div>
-          </div>
-          <div class="card-body bg-primary m-3">
-            <h5 class="card-title text-white">${listing.title}</h5>
-            <p class="card-text text-white-50">
-            ${listing.description}
-            </p>
-          </div>
-          <div class="card-footer bg-primary border-0 m-3">
-            <div><small class="text-danger"> AUCTION ENDS: ${new Date(listing.endsAt).toLocaleDateString()} @ ${new Date(listing.endsAt).toLocaleTimeString()}</small></div>
-            <div><small class="text-muted"> LAST UPDATED: ${new Date(listing.updated).toLocaleDateString()}</small></div>
-          </div>
-          
+            <div class="card-body bg-primary m-3">
+              <h5 class="card-title text-white">${listing.title}</h5>
+              <p class="card-text text-white-50">
+              ${listing.description}
+              </p>
+            </div>
+            <div class="card-footer bg-primary border-0 m-3">
+              <div><small class="text-danger"> AUCTION ENDS: ${new Date(listing.endsAt).toLocaleDateString()} @ ${new Date(listing.endsAt).toLocaleTimeString()}</small></div>
+              <div><small class="text-muted"> LAST UPDATED: ${new Date(listing.updated).toLocaleDateString()}</small></div>
+            </div>
+          </a>
         </div>
       </div>
           
