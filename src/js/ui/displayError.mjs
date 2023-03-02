d8459890c3719d43ed6f82fd8d05b61c05749f14
VaNomad <sjur.hassel@gmail.com>
2023-03-02 22:35:10 +0000

    Adds bidError msg in displayError.mjs

--- src/js/ui/displayError.mjs
+++ src/js/ui/displayError.mjs
@@ -154,4 +154,23 @@ export function searchError() {
   return msg;
             
 }
+export function bidError() {
+  const msg = document.createElement("div");
+  msg.innerHTML = `
+
+  <div class="card bg-primary bg-opacity-75 border-danger banner-h1 text-center p-5 ms-3 mb-5">
+                            <div class="p-3">
+                              <h5 class="fs-4 fw-bold text-danger">THE SERVER CAN NOT AUTHORIZE THIS BID!</h5>
+                            </div>
+                            <div class="p-1">
+                              <img src="/assets/vectors/heartLogo_logout.png" height="60">
+                            </div>
+                            <div class="p-3">
+                              <h5 class="fs-4 fw-bold text-warning">PLEASE TRY PLACING YOUR BID AGAIN</h5>
+                            </div>
+
+  `;
+  return msg;
+            
+}
 
