a83b93e7f666349f36cb3cc3d72ed9f99c75965f
VaNomad <sjur.hassel@gmail.com>
2023-03-02 22:34:29 +0000

    Adds bidMsg + correct innerHTML image targets

--- src/js/pages/specific.mjs
+++ src/js/pages/specific.mjs
@@ -188,23 +188,23 @@ export async function specificPreview() {
          <div class="container-lg text-center text-white">
            <div class="row bg-secondary rounded mb-3">
              <div class="col-lg">
-               <div class="d-flex justify-content-center pt-5">
-                 <div class="big-img d-flex justify-content-center align-items-center bg-transparent">
-                 ${results.media[0]}
-                </div>
+                <div class="d-flex justify-content-center pt-5">
+                  <div class="d-flex justify-content-center align-items-center bg-transparent">
+                    <img src="${results.media[0]}" class="big-img" alt="image 1 of listing">
+                  </div>
               </div>
               <div class="d-flex justify-content-evenly pt-4">
                 <div class="d-flex justify-content-center align-items-center ms-5">
-                  ${results.media[1]}
+                  <img src="${results.media[1]}" class="small-img" alt="image 2 of listing">
                 </div>
                 <div class="d-flex justify-content-center align-items-center">
-                  <img class="small-img" src="" alt="Picture of Object" />
+                  <img src="${results.media[2]}" class="small-img" alt="image 3 of listing">
                 </div>
                 <div class="d-flex justify-content-center align-items-center">
-                  <img class="small-img" src="" alt="Picture of Object" />
+                  <img src="${results.media[3]}" class="small-img" alt="image 4 of listing">
                 </div>
                 <div class="d-flex justify-content-center align-items-center me-5">
-                  <img class="img-fluid" width="30" height="30" src="" alt="Picture of Object" />
+                  <img src="${results.media[4]}" class="small-img" alt="image 5 of listing">
                 </div>
               </div>
             </div>
@@ -225,8 +225,8 @@ export async function specificPreview() {
                       <div class="input-group">
                         <span class="input-group-text bg-secondary"><img src="/assets/vectors/heartLogo_green.png"
                             height="30" alt="" /></span>
-                        <input type="text" class="form-control" id="bidAmount"
-                          aria-label="Amount (to the nearest dollar)" />
+                        <input type="number" name="number" class="form-control" id="bidAmount"
+                          aria-label="Amount (to the nearest cybid credit and dollar)" />
                         <span class="input-group-text bg-secondary text-white">.00</span>
                       </div>
                     </form>
@@ -238,7 +238,7 @@ export async function specificPreview() {
                   </div>
                   <div class="expiration d-flex justify-content-between align-items-center px-2">
                     <p>CURRENT BID</p>
-                    ${results.title}
+                    ${results._count[0]}
                   </div>
                   <div class="p-2">
                     <button class="w-100 px-5 py-2 btn btn-lg btn-outline-success banner-h1 fs-2" type="submit">
